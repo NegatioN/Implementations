230e6fc45f26cc74f7ccc0968173051c7c86943a
NegatioN <joakimrishaug@gmail.com>
2022-09-23 18:01:58 +0000

    some more stuff

--- attention/run.py
+++ attention/run.py
@@ -12,29 +12,32 @@ from torch.nn import functional as F
 
 
 def attention(queries, keys, values, scale):
-    x = (queries @ keys) / scale
-    return F.softmax(x) * values
+    x = (queries @ keys.T) / scale
+    return F.softmax(x) @ values
 
+#TODO add batching?
 class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_dim):
+    def __init__(self, emb_dim, seq_len):
         super(MultiHeadAttention, self).__init__()
+        # TODO initialization scheme for parameters
         self.values = nn.Parameter(torch.randn((emb_dim, emb_dim)))
         self.queries = nn.Parameter(torch.randn((emb_dim, emb_dim)))
         self.keys = nn.Parameter(torch.randn((emb_dim, emb_dim)))
+        self.fc = nn.Linear(seq_len * emb_dim, emb_dim, bias=False)
         self.scale = math.sqrt(emb_dim)
     
     def forward(self, inp):
+        #assert len(inp.size()) == 2, f'Input is not of dim=2, but dim={len(inp.size())}'
         v = inp @ self.values
         q = inp @ self.queries
         k = inp @ self.keys
-        return attention(q, k, v, self.scale)
+        x = attention(q, k, v, self.scale).view(-1)
+        return self.fc(x)
 
 
 n, emb_dim = 3, 10
-vec = torch.randn(emb_dim)
-a = MultiHeadAttention(emb_dim)
+vec = torch.randn(n, emb_dim)
+a = MultiHeadAttention(emb_dim, n)
 
 x = a(vec)
 x
-
-nn.MultiheadAttention??
