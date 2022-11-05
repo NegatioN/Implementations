80a349509d82f3b6be432260cd9b0ef8c27c21f1
NegatioN <joakimrishaug@gmail.com>
2022-11-05 20:53:45 +0100

    add micrograd in scala

---
+++ micrograd/src/Main.scala
@@ -0,0 +1,52 @@
+object Main extends App {
+  case class Value(value: Float, children: Set[Value] = Set.empty) {
+    var grad = 0.0f
+    val prev: Set[Value] = children
+    var _backward: () => Unit = () => {}
+
+    def *(that: Value): Value = {
+      val result = Value(value * that.value, Set(this, that))
+      def _backward() = {
+        this.grad += result.grad * that.value
+        that.grad += result.grad * this.value
+      }
+      result._backward = _backward
+      result
+    }
+
+    def +(that: Value): Value = {
+      val result = Value(value + that.value, Set(this, that))
+      def _backward() = {
+        this.grad += result.grad
+        that.grad += result.grad
+      }
+      result._backward = _backward
+      result
+    }
+
+    def -(that: Value): Value = this.+(that * Value(-1.0f)) // This creates new nodes in the graph, but will suffice
+
+    def backward = {
+      grad = 1.0f
+      var stack = List(this)
+      while (stack.nonEmpty) {
+        val node = stack.head
+        stack = stack.tail
+        node._backward()
+        stack = stack ++ node.prev
+      }
+  }
+
+    override def toString: String = f"Value($value%.2f, grad=$grad%.2f)"
+  }
+
+  val a = Value(5.0f)
+  val b = Value(3.0f)
+  val c = Value(3.0f)
+
+  val d = a + b
+  val e = d * c
+  e.backward
+
+  println(a, b, c, d, e)
+}
